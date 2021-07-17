5eaea12923b90b25e805c764469b60b64536fbf2
wilsesa <wilson.sanchez14@hotmail.com>
2021-07-17 11:49:30 -0300

    Validacao CPF

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace ValidacaoCPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPF.Location = new System.Drawing.Point(41, 26);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(129, 20);
+            this.lblCPF.TabIndex = 0;
+            this.lblCPF.Text = "Digite um CPF:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resultado:";
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(213, 28);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(150, 20);
+            this.txtCPF.TabIndex = 2;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(257, 124);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(106, 38);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(434, 189);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCPF);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ ValidacaoCPF.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacaoCPF
+{
+    class ValidacaoCPF
+    {
+        public static bool Verificar(string cpf)
+        {
+            int[] VerificarDigito1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] VerificarDigito2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string CPF1, CPF2;
+            int Soma, Resultado;
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+
+            //CPF informado por o usuário
+            CPF1 = cpf.Substring(0, 10);
+            Soma = 0;
+
+            for (int i = 0; i < 9; i++)
+
+                Soma += Convert.ToInt32(CPF1[i].ToString()) * VerificarDigito1[i];
+
+            Resultado = Soma % 11;
+
+            if (Resultado < 2)
+            {
+                Resultado = 0;
+            }
+            else
+            {
+                Resultado = 11 - Resultado;
+            }
+
+            CPF2 = Resultado.ToString();
+            CPF1 = CPF1 + CPF2;
+
+            Soma = 0;
+            for (int x = 0; x < 10; x++)
+
+                Soma += Convert.ToInt32(CPF1[x].ToString()) * VerificarDigito2[x];
+
+            Resultado = Soma % 11;
+
+            if (Resultado < 2)
+            {
+                Resultado = 0;
+            }
+            else
+            {
+                Resultado = 11 - Resultado;
+            }
+
+            CPF2 = CPF2 + Resultado.ToString();
+
+            return cpf.EndsWith(CPF2);
+
+
+        }
+    }
+}
